9103af481ba98db52ae710ba3ad52e2f54156c19
HawkTom <577254570@qq.com>
2017-09-03 20:16:31 +0800

    Initial commit

--- py/Tree/HUFFMAN.py
+++ py/Tree/HUFFMAN.py
@@ -1,14 +1,98 @@
 
-class queue(object):
-    def __init__(self):
-        self.que = []
+class TreeNode(object):
+    def __init__(self, data):
+        self.val = data[0]
+        self.priority = data[1]
+        self.leftChild = None
+        self.rightChild = None
+        self.code = ""
+
+def creatnodeQ(codes):
+    q = []
+    for code in codes:
+        q.append(TreeNode(code))
+    return q
+
+def addQ(queue, nodeNew):
+    if len(queue) == 0:
+        return [nodeNew]
+    for i in range(len(queue)):
+        if queue[i].priority >= nodeNew.priority:
+            return queue[:i] + [nodeNew] + queue[i:]
+    return queue + [nodeNew]
+
+class nodeQeuen(object):
+
+    def __init__(self, code):
+        self.que = creatnodeQ(code)
         self.size = len(self.que)
 
-    def input(self,element):
-        self.que.append(element)
+    def addNode(self,node):
+        self.que = addQ(self.que, node)
         self.size += 1
 
-    def output(self):
-        self.que.pop(0)
+    def popNode(self):
         self.size -= 1
+        return self.que.pop(0)
+
+def freChar(string):
+    d ={}
+    for c in string:
+        if not c in d:
+            d[c] = 1
+        else:
+            d[c] += 1
+    return sorted(d.items(),key=lambda x:x[1])
+
+def creatHuffmanTree(nodeQ):
+    while nodeQ.size != 1:
+        node1 = nodeQ.popNode()
+        node2 = nodeQ.popNode()
+        r = TreeNode([None, node1.priority+node2.priority])
+        r.leftChild = node1
+        r.rightChild = node2
+        nodeQ.addNode(r)
+    return nodeQ.popNode()
+
+codeDic1 = {}
+codeDic2 = {}
+def HuffmanCodeDic(head, x):
+    global codeDic, codeList
+    if head:
+        HuffmanCodeDic(head.leftChild, x+'0')
+        head.code += x
+        if head.val:
+            codeDic2[head.code] = head.val
+            codeDic1[head.val] = head.code
+        HuffmanCodeDic(head.rightChild, x+'1')
+
+def TransEncode(string):
+    global codeDic1
+    transcode = ""
+    for c in string:
+        transcode += codeDic1[c]
+    return transcode
+
+def TransDecode(StringCode):
+    global codeDic2
+    code = ""
+    ans = ""
+    for ch in StringCode:
+        code += ch
+        if code in codeDic2:
+            ans += codeDic2[code]
+            code = ""
+    return ans
+
+
+string = "AAGGDCCCDDDGFBBBFFGGDDDDGGGEFFDDCCCCDDFGAAA"
+t = nodeQeuen(freChar(string))
+tree = creatHuffmanTree(t)
+HuffmanCodeDic(tree, '')
+print(codeDic1,codeDic2)
+a = TransEncode(string)
+print(a)
+aa = TransDecode(a)
+print(aa)
+print(string == aa)
 
